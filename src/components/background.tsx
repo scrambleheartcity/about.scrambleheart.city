c5c4e8c7e7c87a57e973c5ff5dd2b99a1f81e4dc
mpaulweeks <mpaulweeks@gmail.com>
2024-08-25 10:24:30 -0400

    prettier

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React from 'react';
 
-export function Background({ img, children }: { img: string | undefined } & React.PropsWithChildren) {
+export function Background({
+  img,
+  children,
+}: { img: string | undefined } & React.PropsWithChildren) {
   return (
     <div
       className="min-h-screen w-full h-full bg-cover bg-center flex flex-col justify-center items-center"
       style={{
         backgroundImage: img ? `url(${img})` : undefined,
       }}
-    >{children}</div>
+    >
+      {children}
+    </div>
   );
 }
