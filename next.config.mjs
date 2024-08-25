0d0dab72ebb821c109e6afc824c3e50dc2ec74e3
mpaulweeks <mpaulweeks@gmail.com>
2024-08-25 10:02:22 -0400

    next/github setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // fixes imports
+  trailingSlash: true,
+  // adds export behavior to "next build"
+  output: "export",
+};
 
 export default nextConfig;
