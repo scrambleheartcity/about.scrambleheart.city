141b19cb04468b6bbc9076de7c7789e890e746fb
mpaulweeks <mpaulweeks@gmail.com>
2024-09-27 10:16:53 -0400

    fix font and media query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: 'device-width',
+};
 
 export const metadata: Metadata = {
   title: 'Scramble Heart City',
@@ -18,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
