c8b57ef929089fbbdf1edad2fec395471e47e666
mpaulweeks <mpaulweeks@gmail.com>
2024-09-29 12:43:40 -0400

    draft lower section with info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,21 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main>
+      <aside className={styles.socials}>
+        {SocialLinks.map(link => (
+          <ExternalLink key={link.url} href={link.url}>
+            <img
+              src={link.icon}
+              className={classCat(styles.socialIcon, styles.hover)}
+              alt={link.label}
+            />
+          </ExternalLink>
+        ))}
+      </aside>
       <Background
         img="assets/beach_day_1080.jpg"
         // video="assets/replay-2024-08-23-[480p].mp4"
       >
-        <aside className={styles.socials}>
-          {SocialLinks.map(link => (
-            <ExternalLink key={link.url} href={link.url}>
-              <img
-                src={link.icon}
-                className={classCat(styles.socialIcon, styles.hover)}
-                alt={link.label}
-              />
-            </ExternalLink>
-          ))}
-        </aside>
         <section className={styles.content}>
           <img src="assets/scramble_logo.png" className={styles.logo} />
           {ProjectLinks.map(link => (
@@ -33,6 +33,33 @@ export default function Home() {
           ))}
         </section>
       </Background>
+      <section className={styles.info}>
+        <div className={styles.infoCopy}>
+          <h1>
+            The newest game from the creators of{' '}
+            <ExternalLink href="https://about.toughlovearena.com">
+              Tough Love Arena
+            </ExternalLink>
+            !
+          </h1>
+          <ul>
+            <li>Exciting 3v3 tag team fighting action</li>
+            <li>Playable in the browser, no installation required</li>
+            <li>Closed Alpha - Late 2024</li>
+            <li>Early Access - 2025</li>
+          </ul>
+        </div>
+        <div>
+          <video
+            className={styles.video}
+            autoPlay={true}
+            muted={true}
+            loop={true}
+          >
+            <source src="assets/replay-2024-08-23-[480p].mp4"></source>
+          </video>
+        </div>
+      </section>
     </main>
   );
 }
