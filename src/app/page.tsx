ed3c17695a20818d671177e559197db5c6322a14
mpaulweeks <mpaulweeks@gmail.com>
2024-10-03 14:34:58 -0400

    feedback and pairing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.page}>
       <aside className={styles.socials}>
         {SocialLinks.map(link => (
           <ExternalLink key={link.url} href={link.url}>
@@ -33,40 +33,59 @@ export default function Home() {
           ))}
         </section>
       </Background>
-      <section className={styles.info}>
+      <div className={classCat(styles.tilt, styles.info)}>
+        <section className={styles.info}>
+          <div className={styles.left}>
+            <div className={styles.infoCopy}>
+              <header>An in-browser 3v3 tag team fighting game</header>
+              <div>
+                From the team behind{' '}
+                <ExternalLink href="https://about.toughlovearena.com">
+                  Tough Love Arena
+                </ExternalLink>{' '}
+                comes Scramble Heart City, an exciting, action-packed adventure
+                that's playable in browser — no installation required!
+              </div>
+            </div>
+          </div>
+          <div className={styles.right}>
+            <video
+              className={styles.video}
+              preload="none"
+              autoPlay={true}
+              muted={true}
+              loop={true}
+            >
+              <source src="assets/video/replay-2024-08-23-[480p].mp4" />
+            </video>
+          </div>
+        </section>
+      </div>
+      <section className={styles.dates}>
         <div className={styles.left}>
-          <div className={styles.infoCopy}>
-            <h2>
-              From the team behind{' '}
-              <ExternalLink href="https://about.toughlovearena.com">
-                Tough Love Arena
-              </ExternalLink>
-              !
-            </h2>
-            <ul>
-              <li>Exciting 3v3 tag team fighting action</li>
-              <li>Playable in the browser, no installation required</li>
-              <li>Closed Alpha - Late 2024</li>
-              <li>Early Access - 2025</li>
-            </ul>
-          </div>
-          <div className={styles.infoSprites}>
-            <img src="assets/sprites/sword_1.png" />
-            <img src="assets/sprites/sword_2.png" className={styles.mirror} />
-          </div>
+          <img src="assets/sprites/sword_1.png" />
         </div>
         <div className={styles.right}>
-          <video
-            className={styles.video}
-            preload="none"
-            autoPlay={true}
-            muted={true}
-            loop={true}
-          >
-            <source src="assets/video/replay-2024-08-23-[480p].mp4"></source>
-          </video>
+          <div className={styles.dateCopy}>
+            <header>Coming soon</header>
+            <div className={styles.dateCols}>
+              <div className={styles.left}>
+                <div>
+                  <b>Closed Alpha</b>
+                </div>
+                <div>Late 2024</div>
+              </div>
+              <div className={styles.right}>
+                <div>
+                  <b>Early Access</b>
+                </div>
+                <div>2025</div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
+      <footer>© M. Paul Games LLC</footer>
     </main>
   );
 }
