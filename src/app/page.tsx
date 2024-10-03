c7852c86674d32acdf0155015803cd55bb5ceab3
mpaulweeks <mpaulweeks@gmail.com>
2024-10-03 18:07:16 -0400

    progress on column

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         img="assets/bg/rooftop_v2.jpg"
         // video="assets/video/replay-2024-08-23-[480p].mp4"
       >
-        <section className={styles.content}>
+        <section className={styles.hero}>
           <img
             src="assets/scramble_logo.png"
             className={classCat(styles.logo, styles.fader)}
@@ -44,62 +44,66 @@ export default function Home() {
         </section>
       </Background>
       <div className={classCat(styles.tilt, styles.info)}>
-        <Column>
-          <section className={styles.info}>
-            <div className={styles.left}>
-              <div className={styles.infoCopy}>
-                <header>An in-browser 3v3 tag team fighting game</header>
-                <div>
-                  From the team behind{' '}
-                  <ExternalLink href="https://about.toughlovearena.com">
-                    Tough Love Arena
-                  </ExternalLink>{' '}
-                  comes Scramble Heart City, an exciting, action-packed
-                  adventure that's playable in browser — no installation
-                  required!
+        <section className={styles.info}>
+          <Column>
+            <div className={styles.content}>
+              <div className={styles.left}>
+                <div className={styles.infoCopy}>
+                  <header>An in-browser 3v3 tag team fighting game</header>
+                  <div>
+                    From the team behind{' '}
+                    <ExternalLink href="https://about.toughlovearena.com">
+                      Tough Love Arena
+                    </ExternalLink>{' '}
+                    comes Scramble Heart City, an exciting, action-packed
+                    adventure that's playable in browser — no installation
+                    required!
+                  </div>
                 </div>
               </div>
+              <div className={styles.right}>
+                <video
+                  className={styles.video}
+                  preload="none"
+                  autoPlay={true}
+                  muted={true}
+                  loop={true}
+                >
+                  <source src="assets/video/replay-2024-08-23-[480p].mp4" />
+                </video>
+              </div>
+            </div>
+          </Column>
+        </section>
+      </div>
+      <section className={styles.dates}>
+        <Column>
+          <div className={styles.content}>
+            <div className={styles.left}>
+              <img src="assets/sprites/sword_1.png" />
             </div>
             <div className={styles.right}>
-              <video
-                className={styles.video}
-                preload="none"
-                autoPlay={true}
-                muted={true}
-                loop={true}
-              >
-                <source src="assets/video/replay-2024-08-23-[480p].mp4" />
-              </video>
-            </div>
-          </section>
-        </Column>
-      </div>
-      <Column>
-        <section className={styles.dates}>
-          <div className={styles.left}>
-            <img src="assets/sprites/sword_1.png" />
-          </div>
-          <div className={styles.right}>
-            <div className={styles.dateCopy}>
-              <header>Coming soon</header>
-              <div className={styles.dateCols}>
-                <div className={styles.left}>
-                  <div>
-                    <b>Closed Alpha</b>
+              <div className={styles.dateCopy}>
+                <header>Coming soon</header>
+                <div className={styles.dateCols}>
+                  <div className={styles.left}>
+                    <div>
+                      <b>Closed Alpha</b>
+                    </div>
+                    <div>Late 2024</div>
                   </div>
-                  <div>Late 2024</div>
-                </div>
-                <div className={styles.right}>
-                  <div>
-                    <b>Early Access</b>
+                  <div className={styles.right}>
+                    <div>
+                      <b>Early Access</b>
+                    </div>
+                    <div>2025</div>
                   </div>
-                  <div>2025</div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </Column>
+        </Column>
+      </section>
       <footer>© M. Paul Games LLC</footer>
     </main>
   );
