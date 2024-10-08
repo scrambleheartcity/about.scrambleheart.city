b015533881079f3c9c4d458559952aeed4291c64
mpaulweeks <mpaulweeks@gmail.com>
2024-10-08 12:25:27 -0400

    <Background/> scales with content size, now set to 95vh

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -5,9 +5,9 @@ import { classCat } from './util';
 export function Background(props: React.PropsWithChildren) {
   return (
     <div className={styles.wrapper}>
-      <div className={classCat(styles.image, styles.bg1)}></div>
-      <div className={classCat(styles.image, styles.bg2)}></div>
-      <div className={classCat(styles.image, styles.bg3)}></div>
+      <div className={classCat(styles.imageHolder, styles.bg1)}></div>
+      <div className={classCat(styles.imageHolder, styles.bg2)}></div>
+      <div className={classCat(styles.imageHolder, styles.bg3)}></div>
       <div className={styles.content}>{props.children}</div>
     </div>
   );
