89f8e1dbb59fe6c7faf202b3a6e1ed2fc969dd1a
mpaulweeks <mpaulweeks@gmail.com>
2024-10-24 15:39:16 -0400

    webgpu detect with more info

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -5,27 +5,59 @@ import { WebGpuError } from '@/components/webgpu';
 import { useEffect, useState } from 'react';
 import styles from './play.module.css';
 
+const performRedirect = false;
 const target =
   'https://play.void.dev/mpaulweeks/scramble-heart-city/preview/playtest/';
 
+enum WebGPU {
+  Checking = 1,
+  Detected,
+  Missing,
+}
+
 export default function Playtest() {
-  const [error, setError] = useState(false);
+  const [webgpu, setWebGPU] = useState(WebGPU.Checking);
+  const [userAgent, setUserAgent] = useState<string>();
 
   useEffect(() => {
     (async () => {
       const webgpu = await detectWebGPU();
       if (webgpu) {
-        const search = window.location.search;
-        window.location.href = target + search;
+        setWebGPU(WebGPU.Detected);
+        if (performRedirect) {
+          const search = window.location.search;
+          window.location.href = target + search;
+        }
       } else {
-        setError(true);
+        setWebGPU(WebGPU.Missing);
       }
+      setUserAgent(navigator.userAgent);
     })();
   }, []);
 
   return (
     <main className={styles.page}>
-      {error ? <WebGpuError /> : <h1>checking for WebGPU...</h1>}
+      <div>
+        {
+          {
+            [WebGPU.Checking]: <h1>checking for WebGPU...</h1>,
+            [WebGPU.Detected]: (
+              <h1>
+                WebGPU detected!
+                <br />
+                you're all set for the playtest
+              </h1>
+            ),
+            [WebGPU.Missing]: <WebGpuError />,
+          }[webgpu]
+        }
+        {userAgent ? (
+          <div className={styles.userAgent}>
+            <h1>debug info</h1>
+            <div>{userAgent}</div>
+          </div>
+        ) : null}
+      </div>
     </main>
   );
 }
