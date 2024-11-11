471214a49b29803a325b362cd49f02a617db50b5
mpaulweeks <mpaulweeks@gmail.com>
2024-11-11 01:17:17 -0500

    swap city for still hero image

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -5,9 +5,10 @@ import { classCat } from './util';
 export function Background(props: React.PropsWithChildren) {
   return (
     <div className={styles.wrapper}>
-      <div className={classCat(styles.imageHolder, styles.bg1)}></div>
-      <div className={classCat(styles.imageHolder, styles.bg2)}></div>
-      <div className={classCat(styles.imageHolder, styles.bg3)}></div>
+      <div className={classCat(styles.imageHolder, styles.bg0)}></div>
+      {/* <div className={classCat(styles.imageHolder, styles.bg1)}></div> */}
+      {/* <div className={classCat(styles.imageHolder, styles.bg2)}></div> */}
+      {/* <div className={classCat(styles.imageHolder, styles.bg3)}></div> */}
       <div className={styles.content}>{props.children}</div>
     </div>
   );
