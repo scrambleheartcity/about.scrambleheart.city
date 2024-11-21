cc143838f5d36559fd6ef6749b5d72c05e9e5b96
mpaulweeks <mpaulweeks@gmail.com>
2024-11-21 14:44:06 -0500

    util

--- src/components/util.ts
+++ src/components/util.ts
@@ -2,6 +2,15 @@ export function classCat(...args: (string | undefined)[]) {
   return args.flatMap(e => (e ? [e] : [])).join(' ');
 }
 
+export function range(a: number, b?: number, increment?: number): number[] {
+  const [start, end] = b === undefined ? [0, a] : [a, b];
+  const out: number[] = [];
+  for (let i = start; i < end; i += increment ?? 1) {
+    out.push(i);
+  }
+  return out;
+}
+
 export async function detectWebGPU(): Promise<boolean> {
   try {
     const webgpu = await navigator.gpu.requestAdapter();
