c38ee5811164fbd9fb79e17cae45a2b76332056c
mpaulweeks <mpaulweeks@gmail.com>
2025-04-08 14:47:18 -0400

    smaller social preview for frame data page

--- src/app/framedata/page.tsx
+++ src/app/framedata/page.tsx
@@ -1,4 +1,21 @@
+import { OpenGraphConfig } from '@/components/openGraph';
+import { Metadata } from 'next';
+import { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types';
 import { FrameData } from './framedata';
+
+const playGraph: OpenGraph = {
+  ...OpenGraphConfig,
+  title: 'Scramble Heart City - Frame Data',
+  images: [],
+};
+export const metadata: Metadata = {
+  title: playGraph.title,
+  description: playGraph.description,
+  icons: {
+    shortcut: '/assets/Grenade800.png',
+  },
+  openGraph: playGraph,
+};
 
 export default function FrameDataPage() {
   return <FrameData />;
