23b6603c80ae06e1ac761f61f902e86f2088cc82
mpaulweeks <mpaulweeks@gmail.com>
2025-06-04 17:04:15 -0400

    fix urls

--- src/app/data.ts
+++ src/app/data.ts
@@ -7,8 +7,8 @@ export type SocialLink = Link & {
 };
 
 const HostedUrl = `https://playtest.scrambleheart.city`;
-export const ProdUrl = `${HostedUrl}/main`;
-export const PlaytestUrl = `${HostedUrl}/playtest`;
+export const ProdUrl = HostedUrl;
+export const PlaytestUrl = HostedUrl;
 export const FeatureFlagsUrl = `https://shc-matchmaker-default-rtdb.firebaseio.com/flags.json`;
 
 export const DiscordUrl = 'https://discord.gg/6hbrfUgZJB';
