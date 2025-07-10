d548496d6f718f9d4096bcdab1c6146683c424cb
mpaulweeks <mpaulweeks@gmail.com>
2025-07-10 15:15:35 -0400

    require multiple playtest flags

--- src/hooks/usePlaytestStatus.ts
+++ src/hooks/usePlaytestStatus.ts
@@ -2,18 +2,26 @@ import { FeatureFlagsUrl } from '@/app/data';
 import { useFetch } from './useFetch';
 import { useQueryParam } from './useQueryParam';
 
+type DbFeatureFlags = {
+  playtest?: boolean;
+  playtest_visible?: boolean;
+};
+
 export function usePlaytestStatus() {
   const forceActive = useQueryParam('access');
-  const featureFlags = useFetch(
+  const featureFlagsStr = useFetch(
     `${FeatureFlagsUrl}?cacheBust=${new Date().getTime()}`,
   );
+  const featureFlags: DbFeatureFlags | null = featureFlagsStr
+    ? JSON.parse(featureFlagsStr)
+    : null;
   const playtestActive: boolean =
     forceActive === 'false'
       ? false
       : forceActive === 'true'
       ? true
       : featureFlags
-      ? !!JSON.parse(featureFlags).playtest
+      ? !!featureFlags.playtest && !!featureFlags.playtest_visible
       : false;
   return playtestActive;
 }
