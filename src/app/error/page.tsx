e592806792b74b105bc805f3eb96792bc2fffdb3
mpaulweeks <mpaulweeks@gmail.com>
2026-05-19 16:34:03 -0400

    remove playtest active check from error page

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import { VertPage } from '@/components/vertPage';
-import { usePlaytestStatus } from '@/hooks/usePlaytestStatus';
 import { useQueryParam } from '@/hooks/useQueryParam';
 import { PlaytestUrl } from '../data';
 import styles from '../play/play.module.css';
 
 export default function ErrorPage() {
-  const playtestActive = usePlaytestStatus();
   const passThru = useQueryParam('passThrough');
   const errorHead = useQueryParam('head') ?? '';
   const errorBody = useQueryParam('body') ?? '';
@@ -39,20 +37,18 @@ ${errorBody}
 
       <section>
         <p>{messageToPlayer}</p>
-        {playtestActive && (
-          <>
-            {passThru && (
-              <p>
-                <a href={`${PlaytestUrl}/?${passThru}`}>
-                  Restart the game with new settings
-                </a>
-              </p>
-            )}
+        <>
+          {passThru && (
             <p>
-              <a href={`${PlaytestUrl}`}>Restart the game</a>
+              <a href={`${PlaytestUrl}/?${passThru}`}>
+                Restart the game with new settings
+              </a>
             </p>
-          </>
-        )}
+          )}
+          <p>
+            <a href={`${PlaytestUrl}`}>Restart the game</a>
+          </p>
+        </>
       </section>
     </VertPage>
   );
